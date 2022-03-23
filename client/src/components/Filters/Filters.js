08cfbd7fd730977b4653b15dd2e61d8ba71dd665
Jose Ramirez <josemramirez81@outlook.com>
2022-03-23 12:25:44 -0400

    Create filters container component

---
+++ client/src/components/Filters/Filters.js
@@ -0,0 +1,22 @@
+import './Filters.scss'
+import {useState} from "react";
+import {FiltersList} from "../FiltersList/FiltersList";
+
+export const Filters = ({ location, history}) => {
+  const [openList, setOpenList] = useState(false)
+
+  const showMenu = () => {
+    setOpenList(!openList)
+  }
+
+  return(
+    <div className="filters">
+      <div className="filters__header"><h2 className="filters__header__heading">Filters</h2></div>
+      <div className="filters__header filters__header--button" onClick={showMenu}>
+        <h2 className="filters__header__heading">Filters</h2>
+        <span className={`material-icons filters__header__icon ${openList? 'filters__header__icon--open': ''}`}>arrow_forward_ios</span>
+      </div>
+      <FiltersList location={location} open={openList}/>
+    </div>
+  )
+}
