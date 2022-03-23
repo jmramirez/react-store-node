cc990b7a531f0baaf6f6176b4bedc4c3644fa64b
Jose Ramirez <josemramirez81@outlook.com>
2022-03-23 10:33:12 -0400

    Add Initial client files

--- server/src/config/environment.js
+++ server/src/config/environment.js
@@ -1,6 +1,6 @@
 export default {
   env: process.env.NODE_ENV || 'development',
-  port: parseInt(process.env.PORT) || 3000,
+  port: parseInt(process.env.PORT) || 8080,
   jwtSecret:
     process.env.JWT_SECRET ||
     '7231563d1c75ced86ff39ff7bc276b2da977491f43f7d6a074f1a3d19b60b04b',
