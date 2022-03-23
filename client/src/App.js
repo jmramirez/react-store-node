d749f3163d688cb22a310f5aced192a0423806ec
Jose Ramirez <josemramirez81@outlook.com>
2022-03-23 15:57:31 -0400

    Define products/:slug route

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
 import './App.scss';
 import { BrowserRouter, Routes ,Route} from "react-router-dom";
 import {CatalogPage} from "./pages/CatalogPage/CatalogPage";
+import {ProductPage} from "./pages/ProductPage/ProductPage";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<CatalogPage />}/>
-          <Route path='/products' element={<CatalogPage />}/>
+          <Route path="" element={<CatalogPage />}/>
+          <Route path="products/:slug" element={<ProductPage/>}/>
+          <Route path="products" element={<CatalogPage />}/>
         </Routes>
       </BrowserRouter>
     </div>
