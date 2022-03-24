451618aca05428544d06d80fc9e077dff4415535
Jose Ramirez <josemramirez81@outlook.com>
2022-03-23 21:55:44 -0400

    Create CartPage to show list of ShoppingCart items

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes ,Route} from "react-router-dom";
 import {CatalogPage} from "./pages/CatalogPage/CatalogPage";
 import {ProductPage} from "./pages/ProductPage/ProductPage";
 import {Header} from "./components/Header/Header";
+import {CartPage} from "./pages/CartPage/CartPage";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="" element={<CatalogPage />}/>
           <Route path="products/:slug" element={<ProductPage/>}/>
           <Route path="products" element={<CatalogPage />}/>
+          <Route path="cart" element={<CartPage />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ client/src/pages/CartPage/CartPage.js
@@ -0,0 +1,11 @@
+import './CartPage.scss'
+
+export const CartPage = () => {
+  return (
+    <section className="shoppingCart">
+      <h2 className="shoppingCart__heading">ShoppingCart</h2>
+      <div className="shoppingCart__container">
+      </div>
+    </section>
+  )
+}
