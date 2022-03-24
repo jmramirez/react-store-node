3e157afaf289fb165cb405a7106605322c9f34d2
Jose Ramirez <josemramirez81@outlook.com>
2022-03-24 15:24:49 -0400

    Include modal and userAuth store options

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,22 +2,33 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools} from "redux-devtools-extension";
 import { cartReducer } from "./reducers/cartReducer";
+import {modalReducers} from "./reducers/modalReducer";
+import {userLoginReducer, userRegisterReducer} from "./reducers/userReducer";
 
 const reducer = combineReducers({
   cart: cartReducer,
+  modal: modalReducers,
+  userLogin: userLoginReducer,
+  userRegister: userRegisterReducer
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
+const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null
 
 const initialState = {
   cart: {
     cartItems: cartItemsFromStorage
   },
-  /*modal: {},
-  userLogin: {}*/
+  modal: {
+    showAuthModal: false
+  },
+  userLogin: {
+    userInfo: userInfoFromStorage
+  }
 }
-
 const middleware = [thunk]
 
-const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)))
+const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware))
+)
+
 export default store
