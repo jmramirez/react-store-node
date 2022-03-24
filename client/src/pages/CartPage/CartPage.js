a288d70d99314028ab1965f122f04c6532918442
Jose Ramirez <josemramirez81@outlook.com>
2022-03-24 09:03:24 -0400

    Include cartItemList component

--- client/src/pages/CartPage/CartPage.js
+++ client/src/pages/CartPage/CartPage.js
@@ -1,10 +1,12 @@
 import './CartPage.scss'
+import {CartList} from "../../components/CartList/CartList";
 
 export const CartPage = () => {
   return (
     <section className="shoppingCart">
       <h2 className="shoppingCart__heading">ShoppingCart</h2>
       <div className="shoppingCart__container">
+        <CartList />
       </div>
     </section>
   )
