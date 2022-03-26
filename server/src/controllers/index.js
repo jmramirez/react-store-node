9456abedab6575eabd701b5db2aca3d37e8afde5
Jose Ramirez <josemramirez81@outlook.com>
2022-03-26 01:07:13 -0400

    Setup route to process orders

--- server/src/controllers/index.js
+++ server/src/controllers/index.js
@@ -2,11 +2,15 @@ import { Router } from 'express';
 import filtersRouter from './filter';
 import productsRouter from './product';
 import registerRouter from './register';
+import loginRouter from './login';
+import ordersRouter from './order'
 
 const router = Router();
 
 router.use(registerRouter);
+router.use(loginRouter);
 router.use(filtersRouter);
 router.use(productsRouter);
+router.use(ordersRouter);
 
 export default router;
