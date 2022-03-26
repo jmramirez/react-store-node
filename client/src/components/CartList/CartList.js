c1364687632e455857ec0c7cbbd6b978f13811fa
Jose Ramirez <josemramirez81@outlook.com>
2022-03-26 01:09:58 -0400

    Include methods to verify if user is authenticated

--- client/src/components/CartList/CartList.js
+++ client/src/components/CartList/CartList.js
@@ -1,19 +1,32 @@
 import './CartList.scss'
 import {useDispatch, useSelector} from "react-redux";
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import {useEffect, useState} from "react";
 import {CartItem} from "../CartItem/CartItem";
+import {showAuthModal} from "../../redux/actions/modalActions";
 
 export const CartList = () => {
   const items = useSelector((state) => state.cart.cartItems)
+  const userLogin = useSelector((state) => state.userLogin)
+  const { userInfo } = userLogin
   const [subTotal, setSubTotal] = useState(0)
   const dispatch = useDispatch()
+  const navigate = useNavigate()
 
   useEffect(() => {
     const reducer = (accumulator, cartItem) => accumulator + cartItem.subTotal
     const total = items.reduce(reducer, 0)
     setSubTotal(total)
   }, [items])
+
+  const checkout = () => {
+    if(userInfo){
+      navigate('/checkout')
+    }
+    else {
+      dispatch(showAuthModal('signin'))
+    }
+  }
 
   return(
     <div>
@@ -40,14 +53,25 @@ export const CartList = () => {
         )
         :
         (<p className="cartList__empty">You don't have anything in your cart yet!</p>)
-
-
       }
       <div className="cartList-actions">
         <Link to='/' className="cartList-actions__link-back">
           <span className="material-icons cartList-actions__link-back__icon">arrow_back</span>
           <span className="cartList-actions__link-back__text">Continue Shopping</span>
         </Link>
+        {
+          items.length > 0 &&(
+            <>
+              <div className="cartList-actions__total">
+                <p className="cartList-actions__total__content">Product Total: ${subTotal}</p>
+              </div>
+              <button className="cartList-actions__link-check" onClick={checkout}>
+                <span className="cartList-actions__link-check__text">Chekout</span>
+                <span className="material-icons cartList-actions__link-check__icon">arrow_forward</span>
+              </button>
+            </>
+          )
+        }
       </div>
     </div>
   )
