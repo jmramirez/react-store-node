0330e1b496c204ad3735e11e9513e5252453c42c
Jose Ramirez <josemramirez81@outlook.com>
2022-03-26 01:04:09 -0400

    Add userInfo prop to be use on ProtectedRoutes

--- client/src/App.js
+++ client/src/App.js
@@ -5,9 +5,13 @@ import {ProductPage} from "./pages/ProductPage/ProductPage";
 import {Header} from "./components/Header/Header";
 import {CartPage} from "./pages/CartPage/CartPage";
 import {useSelector} from "react-redux";
+import {CheckoutPage} from "./pages/CheckoutPage/CheckoutPage";
+import {ProtectedRoute} from "./components/ProtectedRoute/ProtectedRoute";
 
 function App() {
   const modalOpen = useSelector((state) => state.modal.showAuthModal)
+  const userLogin = useSelector((state) => state.userLogin)
+  const { userInfo } = userLogin
 
   return (
     <div className="App">
@@ -18,6 +22,7 @@ function App() {
           <Route path="products/:slug" element={<ProductPage/>}/>
           <Route path="products" element={<CatalogPage />}/>
           <Route path="cart" element={<CartPage />} />
+          <Route path="checkout" element={<ProtectedRoute user={userInfo} redirectPath="/cart"><CheckoutPage /></ProtectedRoute>} />
         </Routes>
       </BrowserRouter>
     </div>
