444e162aa62b7515d8443b220888b4223961c987
Jose Ramirez <josemramirez81@outlook.com>
2022-04-12 18:38:18 -0400

    Setup cross-env on server side

--- server/webpack.build.config.js
+++ server/webpack.build.config.js
@@ -9,7 +9,7 @@ module.exports = {
   ],
   output: {
     path: path.join(__dirname, buildDirectory),
-    filename: 'bundle.js',
+    filename: 'store.bundle.js',
     publicPath: '/dist/'
   },
   module: {
